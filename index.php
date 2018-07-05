ea393736061f460b225efdf4599940962ef4b3e1
ElephantFrontend <32238103+ElephantFrontend@users.noreply.github.com>
2018-07-05 15:43:12 +0300

    Первая заливка файлов сайта. Начальная верстка, главная страница

---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>NBK Ukraine</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<!-- header -->
+<header>
+	
+</header>
+<!-- slider -->
+<div class="slider">
+	
+</div>
+<!-- category_product -->
+<div class="category_product">
+	<div class="category_product_wrapper wrapper">
+		<div class="category_product_block">
+		
+		</div>
+		<div class="category_product_block">
+		
+		</div>
+		<div class="category_product_block">
+		
+		</div>
+		<div class="category_product_block">
+		
+		</div>
+		<div class="category_product_block">
+		
+		</div>
+		<div class="category_product_block">
+		
+		</div>
+		<div class="category_product_block">
+		
+		</div>
+		<div class="category_product_block">
+		
+		</div>
+		<div class="category_product_block">
+		
+		</div>
+		<div class="category_product_block">
+		
+		</div>
+	</div>
+</div>
+<!-- parallax -->
+<div class="parallax">
+	<p>Кфтвщь еуче кфтвщь фьше дщкуь шзыгь еуче</p>
+	<p>еуче кфтвщь фьше дщкуь шзыгь еуче Кфтвщь еуче кфтвщь фьше дщкуь шзыгь еуче Кфтвщь еуче кфтвщь фьше дщкуь шзыгь еуче</p>
+	<a href="#"><span>Читати</span> далi</a>
+</div>
+<!-- products -->
+<div class="products">
+	<p><span>Chevron</span> товари</p>
+		<div class="products_wrapper wrapper">
+		<div class="products_block">
+			<div class="news_block_img">
+				
+			</div>
+			<div class="news_block_content">
+				<p>lorem lorem lorem ips int amit lorem</p>
+				<p>lorem ipsum lorem text content amit lorem ipsum lorem</p>
+			</div>
+		</div>
+		<div class="products_block">
+			<div class="news_block_img">
+				
+			</div>
+			<div class="news_block_content">
+				<p>lorem lorem lorem ips int amit lorem</p>
+				<p>lorem ipsum lorem text content amit lorem ipsum lorem</p>
+			</div>
+		</div>
+		<div class="products_block">
+			<div class="news_block_img">
+				
+			</div>
+			<div class="news_block_content">
+				<p>lorem lorem lorem ips int amit lorem</p>
+				<p>lorem ipsum lorem text content amit lorem ipsum lorem</p>
+			</div>
+		</div>
+		<div class="products_block">
+			<div class="news_block_img">
+				
+			</div>
+			<div class="news_block_content">
+				<p>lorem lorem lorem ips int amit lorem</p>
+				<p>lorem ipsum lorem text content amit lorem ipsum lorem</p>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- news -->
+<div class="news">
+	<p><span>Chevron</span> новини</p>
+		<div class="news_wrapper wrapper">
+		<div class="news_block">
+			<div class="news_block_img">
+				
+			</div>
+			<div class="news_block_content">
+				<p>lorem lorem lorem ips int amit lorem</p>
+				<p>lorem ipsum lorem text content amit lorem ipsum lorem</p>
+				<a href="#"><span>Читати</span> далi</a>
+			</div>
+		</div>
+		<div class="news_block">
+			<div class="news_block_img">
+				
+			</div>
+			<div class="news_block_content">
+				<p>lorem lorem lorem ips int amit lorem</p>
+				<p>lorem ipsum lorem text content amit lorem ipsum lorem</p>
+				<a href="#"><span>Читати</span> далi</a>
+			</div>
+		</div>
+		<div class="news_block">
+			<div class="news_block_img">
+				
+			</div>
+			<div class="news_block_content">
+				<p>lorem lorem lorem ips int amit lorem</p>
+				<p>lorem ipsum lorem text content amit lorem ipsum lorem</p>
+				<a href="#"><span>Читати</span> далi</a>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- parallax -->
+<div class="parallax">
+	<p>Кфтвщь еуче кфтвщь фьше дщкуь шзыгь еуче</p>
+	<p>еуче кфтвщь фьше дщкуь шзыгь еуче Кфтвщь еуче кфтвщь фьше дщкуь шзыгь еуче Кфтвщь еуче кфтвщь фьше дщкуь шзыгь еуче</p>
+	<a href="#"><span>Читати</span> далi</a>
+</div>
+<!-- useful materials -->
+<div class="useful_materials">
+	<p><span>Кориснi</span> матерiали</p>
+	<div class="news_wrapper wrapper">
+		<div class="news_block">
+			<div class="news_block_img">
+				
+			</div>
+			<div class="news_block_content">
+				<p>lorem lorem lorem ips int amit lorem</p>
+				<p>lorem ipsum lorem text content amit lorem ipsum lorem</p>
+				<a href="#"><span>Читати</span> далi</a>
+			</div>
+		</div>
+		<div class="news_block">
+			<div class="news_block_img">
+				
+			</div>
+			<div class="news_block_content">
+				<p>lorem lorem lorem ips int amit lorem</p>
+				<p>lorem ipsum lorem text content amit lorem ipsum lorem</p>
+				<a href="#"><span>Читати</span> далi</a>
+			</div>
+		</div>
+		<div class="news_block">
+			<div class="news_block_img">
+				
+			</div>
+			<div class="news_block_content">
+				<p>lorem lorem lorem ips int amit lorem</p>
+				<p>lorem ipsum lorem text content amit lorem ipsum lorem</p>
+				<a href="#"><span>Читати</span> далi</a>
+			</div>
+		</div>
+	</div>
+	<a href="#"><span>Вiдкрити ще</span> Усi кориснi матерiали...</a>
+</div>
+<!-- contacts -->
+<div class="contacts">
+	<p>Контакты</p>
+	<div class="contacts_block">
+		<h3>ТОВАРИСТВО З ОБМЕЖЕННОЮ ВIДПОВIДАЛЬНIСТЮ "НБК УКРАЇНА"</h3>
+		<p>Поштова адреса:</p>
+		<p>01004, м.Київ</p>
+		<p>вул.Червоноармiйська, 9/2, офiс 74</p>
+		<p>Тел: (044) 496 31 30</p>
+		<p>Факс: (044) 496 52 19</p>
+		<p>E-mail: office@nbk-ukr.com</p>
+		<p>Менеджер по роботi з корпоративними клiєнтами</p>
+		<p>Охрiменко Ганна Григорiвна</p>
+		<button class="feedback_button"><span>Надiслати</span> листа</button>
+	</div>
+	<div class="map">
+		<iframe class="map_google" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d635.2575142833881!2d30.520470129251493!3d50.44054079871714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4ceff05ec5a03%3A0xa1b1cdd88000a3ad!2z0LLRg9C70LjRhtGPINCS0LXQu9C40LrQsCDQktCw0YHQuNC70YzQutGW0LLRgdGM0LrQsCwgOS8yLCDQmtC40ZfQsiwgMDIwMDA!5e0!3m2!1sru!2sua!4v1527665735855" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+	</div>
+</div>
+<!-- footer -->
+<footer>
+	<div class="icons">
+		<a href="#"><img src="src/" alt=""></a>
+		<a href="#"><img src="src/" alt=""></a>
+		<a href="#"><img src="src/" alt=""></a>
+		<a href="#"><img src="src/" alt=""></a>
+		<a href="#"><img src="src/" alt=""></a>
+	</div>
+	<p>2018 Всi права захищенi</p>
+</footer>
+
+<!-- scripts -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<script src="script.js"></script>
+</body>
+</html>
